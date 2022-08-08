002c6d2edc52511d06af79e6b2cd4f1e1a634ccb
Watermelon <62407841+StarxSky@users.noreply.github.com>
2022-08-08 17:22:25 +0800

    Update __init__.py

--- Core/__init__.py
+++ Core/__init__.py
@@ -1,3 +1,10 @@
 #
 # © 2022 Apple Inc.
 #
+
+import torch
+
+version = torch.__version__
+device = torch.device('cuda') if torch.cuda.is_available() else 'cpu'
+print(f'PyTorch version :{version}')
+print(f"Device :{device}")
